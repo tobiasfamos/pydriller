515e6ee5c53a8fee716a329ea8ec0c11c81a7e9e
TobiasFamos <tobias.famos@outlook.com>
2020-02-15 14:08:17 +0100

    Add Test Max Statement Added

--- tests/metrics/process/test_statement_count.py
+++ tests/metrics/process/test_statement_count.py
@@ -6,19 +6,25 @@ from pydriller.metrics.process.method_statement_count import MethodStatementCoun
 
 TEST_DATA_SIMPLE = [
     ('test-repos/method-test', None, 'c155c3ee786f40dca1f4e9c59ab989d0b252df80', "Foo.java:Foo::someFunction()",
-     {"sum_statement_added": 6}),
+     {"sum_statement_added": 6,
+      "max_statement_added": 3}),
     ('test-repos/method-test', None, 'c155c3ee786f40dca1f4e9c59ab989d0b252df80', "Foo.java:Foo::someFunction2()",
-     {"sum_statement_added": 6}),
+     {"sum_statement_added": 6,
+      "max_statement_added": 3}),
     ('test-repos/method-test', None, 'c155c3ee786f40dca1f4e9c59ab989d0b252df80', "Foo.java:Foo::someFunction3()",
-     {"sum_statement_added": 6})]
+     {"sum_statement_added": 6,
+      "max_statement_added": 3})]
 
-TEST_DATA_RENAME=[
+TEST_DATA_RENAME = [
     ('test-repos/method-test2', None, "62339f6ae6cfea5b06c54dee1479b9d884afe260", "Bar.java:Foo::someFunction()",
-     {"sum_statement_added": 4}),
+     {"sum_statement_added": 4,
+      "max_statement_added": 3}),
     ('test-repos/method-test2', None, "62339f6ae6cfea5b06c54dee1479b9d884afe260", "Bar.java:Foo::someFunction2()",
-     {"sum_statement_added": 4}),
+     {"sum_statement_added": 4,
+      "max_statement_added": 3}),
     ('test-repos/method-test2', None, "62339f6ae6cfea5b06c54dee1479b9d884afe260", "Bar.java:Foo::someFunction3()",
-     {"sum_statement_added": 4})
+     {"sum_statement_added": 4,
+      "max_statement_added": 3})
 ]
 
 
@@ -30,6 +36,7 @@ def test_simple(path_to_repo, from_commit, to_commit, function_name, expected):
 
     method_count = metric.count()
     assert method_count[function_name] == expected
+
 
 @pytest.mark.parametrize('path_to_repo, from_commit, to_commit, function_name, expected', TEST_DATA_RENAME)
 def test_rename(path_to_repo, from_commit, to_commit, function_name, expected):
