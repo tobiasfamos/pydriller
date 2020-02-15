54f9c561bf8ef8d087d353ea4766b6a1bb864db1
famos <tobias.famos@uzh.ch>
2020-02-15 13:03:53 +0100

    Test for simple statement added sum

---
+++ tests/metrics/process/test_statement_count.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pathlib import Path
+from pydriller.metrics.process.lines_count import LinesCount
+from pydriller.metrics.process.method_statement_count import MethodStatementCount
+
+TEST_DATA = [
+    ('test-repos/method-test', None, 'c155c3ee786f40dca1f4e9c59ab989d0b252df80', "Foo.java:Foo::someFunction()",
+     {"sum_statement_added": 6}),
+    ('test-repos/method-test', None, 'c155c3ee786f40dca1f4e9c59ab989d0b252df80', "Foo.java:Foo::someFunction2()",
+     {"sum_statement_added": 6}),
+    ('test-repos/method-test', None, 'c155c3ee786f40dca1f4e9c59ab989d0b252df80', "Foo.java:Foo::someFunction3()",
+     {"sum_statement_added": 6})
+]
+
+
+@pytest.mark.parametrize('path_to_repo, from_commit, to_commit, function_name, expected', TEST_DATA)
+def test(path_to_repo, from_commit, to_commit, function_name, expected):
+    metric = MethodStatementCount(path_to_repo=path_to_repo,
+                                  from_commit=from_commit,
+                                  to_commit=to_commit)
+
+    method_count = metric.count()
+    assert method_count[function_name] == expected
