93626f85c7eed13deb9137f04cf38a2d9e5487b6
tobiasfamos <tfamos@gmx.ch>
2020-02-21 11:24:49 +0100

    Implement method author count to use method iterator

--- pydriller/metrics/process/method_author_count.py
+++ pydriller/metrics/process/method_author_count.py
@@ -1,36 +1,23 @@
 from pydriller import RepositoryMining
 from pydriller.domain.commit import ModificationType
 from pydriller.metrics.process.process_metric import ProcessMetric
+from pydriller.utils.MethodIterator import get_methods_reversed
 from pydriller.utils.NameGenerator import generate_method_long_name
 
 
 class MethodAuthorCount(ProcessMetric):
 
+    def __init__(self, path_to_repo: str,
+                 from_commit: str,
+                 to_commit: str):
+        super().__init__(path_to_repo, from_commit, to_commit)
+        self.methods = {}
+
     def count(self):
-        methods = {}
-        renamed_files = {}
-
-        for commit in RepositoryMining(path_to_repo=self.path_to_repo,
-                                       from_commit=self.from_commit,
-                                       to_commit=self.to_commit,
-                                       reversed_order=True).traverse_commits():
-
-            for modified_file in commit.modifications:
-
-                file_path = renamed_files.get(modified_file.new_path, modified_file.new_path)
-
-                if modified_file.change_type == ModificationType.RENAME:
-                    renamed_files[modified_file.old_path] = file_path
-
-                file_name = file_path.split("/")[-1]
-
-                for method in modified_file.methods:
-                    if method.statements_deleted or method.statements_added:
-                        method_long_name_with_file = generate_method_long_name(file_name, method.long_name)
-                        MethodAuthorCount.__add_author_to_method_list(methods, commit.author,
-                                                                      method_long_name_with_file)
-
-        return MethodAuthorCount.__create_return_metrics(methods)
+        self.methods = {}
+        for method in get_methods_reversed(self.path_to_repo, self.from_commit, self.to_commit):
+            self.__hanlde_method(method)
+        return MethodAuthorCount.__create_return_metrics(self.methods)
 
     @staticmethod
     def __create_return_metrics(methods):
@@ -44,3 +31,8 @@ class MethodAuthorCount(ProcessMetric):
         method = methods.get(method_long_name_with_file, set())
         method.add(author)
         methods[method_long_name_with_file] = method
+
+    def __hanlde_method(self, method_dto):
+        if method_dto.method.statements_deleted or method_dto.method.statements_added:
+            MethodAuthorCount.__add_author_to_method_list(self.methods, method_dto.commit.author,
+                                                          method_dto.method_long_name)
